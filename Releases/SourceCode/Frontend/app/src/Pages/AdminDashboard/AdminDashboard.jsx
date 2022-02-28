6f19c009d8f99c96a08957e81457d8e32696e620
Wishva Perera <vibushitha99@gmail.com>
2022-02-28 08:02:01 +0530

    initial fronend

---
+++ Releases/SourceCode/Frontend/app/src/Pages/AdminDashboard/AdminDashboard.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AdminDashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>123456</h1>
+      </div>
+    )
+  }
+}
